66f4779cd7c17f1fa3143f2bdc1e21b6370a0680
Propercio Gurgel Guida Junior <pguida@lna.br>
2025-07-08 09:33:05 -0300

    Rotas funcionando

--- workout_api/configs/settings.py
+++ workout_api/configs/settings.py
@@ -1,4 +1,5 @@
-from pydantic import Field, BaseSettings
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DB_URL: str = Field(default='postgresql+asyncpg://workout:workout@localhost/workout')
